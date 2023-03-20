2c91bcfb1a2dbae5cdc96bf39e1012fd53a0c328
somang1206 <dlthakd1206@naver.com>
2023-03-20 17:57:18 +0900

    sec5 완성

--- js/swiper.js
+++ js/swiper.js
@@ -3,18 +3,24 @@ var MainSwiper = new Swiper('.main_swiper .swiper-container', {
         nextEl:'.swiper-button-next',
         prevEl:'.swiper-button-prev',
     },
+    spaceBetween: 0,
     slidesPerView: 3,
     loop : true,
-    centeredSlides: false,
+    centeredSlides: true,
     slideToClickedSlide : true,
+    nested: true,
+    loopedSlides: 1,
 });
 
 
 
 var SubSwiper = new Swiper('.sub_swiper .swiper-container', {
+    spaceBetween: 0,
     loop : true,
     slidesPerView: 1,
-    centeredSlides: false,
+    centeredSlides: true,
+    nested: true,
+    loopedSlides: 1,
 });
 
 
